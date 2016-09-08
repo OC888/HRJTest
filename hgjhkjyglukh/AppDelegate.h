3bafcd58451fa41b57e0a52d5de8fdd0bb3a5adb
OC888 <lx@ldemacbook-pro.local>
2016-09-08 18:46:37 +0800

    Initial Commit

---
+++ hgjhkjyglukh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hgjhkjyglukh
+//
+//  Created by l x on 16/9/8.
+//  Copyright © 2016年 WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hgjhkjyglukh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hgjhkjyglukh
+//
+//  Created by l x on 16/9/8.
+//  Copyright © 2016年 WL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
