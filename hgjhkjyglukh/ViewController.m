b47cec0a958de58e4a1b551cb03f77db1c223316
OC888 <lx@ldemacbook-pro.local>
2016-09-09 11:07:44 +0800

    第一个版本

--- hgjhkjyglukh/ViewController.m
+++ hgjhkjyglukh/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"这是第一个版本");
 }
 
 - (void)didReceiveMemoryWarning {
